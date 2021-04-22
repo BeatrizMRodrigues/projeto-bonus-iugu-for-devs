62d79d43c6284fe1e342117f9b56259e4586d889
beatrizmntr <anabeatrizmonteirorodrigues@gmail.com>
2021-04-21 22:17:39 -0300

    Acrescenta o valor a ser recebido ao JSON e objeto

--- lib/invoice.rb
+++ lib/invoice.rb
@@ -1,13 +1,13 @@
 class Fatura
   
-  attr_accessor :token, :status, :payment_date, :return_date, :payment_type
+  attr_accessor :token, :status, :payment_date, :payment_type, :invoice_amount
 
-  def initialize(token: , status: , payment_date: , return_date: '          ' , payment_type:  )
+  def initialize(token: , status: , payment_date: , payment_type: , invoice_amount: )
     @token = token
     @status = status
     @payment_date = payment_date
-    @return_date = return_date
     @payment_type = payment_type
+    @invoice_amount = invoice_amount
   end
 
   def self.to_convert(fatura)
@@ -15,11 +15,11 @@ class Fatura
     cobranca = Fatura.new(token: cobranca['token'],
                status: cobranca['status'],
                payment_date: cobranca['payment_date'],
-               return_date: cobranca['return_date'],
-               payment_type: cobranca['payment_type'])
+               payment_type: cobranca['payment_type'],
+               invoice_amount: cobranca['invoice_amount'])
 
-    escrita = File.open("data/#{Time.now.to_i}_#{cobranca.payment_type}.txt", 'w+')
-    escrita.write(cobranca.token + cobranca.status + cobranca.payment_type + cobranca.payment_date)
+    escrita = File.open("data/#{Time.now.to_i}_#{cobranca.payment_type}_emissao.txt", 'w+')
+    escrita.write('B' + cobranca.token + cobranca.payment_date + cobranca.payment_type + cobranca.status)
     escrita.close
   end
 end
--- spec/invoice_spec.rb
+++ spec/invoice_spec.rb
@@ -6,32 +6,32 @@ describe Fatura do
     invoice = Fatura.new(token: '7c5ae058526ec4964050649e2b526460b1313afe',
                           status: 'Pendente',
                           payment_date: '25/04/2020',
-                          return_date: '          ',
-                          payment_type: 'Boleto')
+                          payment_type: 'Boleto',
+                          invoice_amount: 'R$ 4500,00')
 
     expect(invoice.class).to eq Fatura
     expect(invoice.token).to eq '7c5ae058526ec4964050649e2b526460b1313afe'
     expect(invoice.status).to eq 'Pendente'
     expect(invoice.payment_date).to eq '25/04/2020'
-    expect(invoice.return_date).to eq '          '
     expect(invoice.payment_type).to eq 'Boleto'
+    expect(invoice.invoice_amount).to eq 'R$ 4500,00'
   end
 
   it 'Converte 1 json em 1 objeto e salva em txt' do
     invoice = Fatura.to_convert('invoices-iugu/fatura001.json')
     
-    expect(File.read("data/#{Time.now.to_i}_Boleto.txt")).to include('b7d933ca217964802cce9abc9a75da96372fbfef')
-    expect(File.read("data/#{Time.now.to_i}_Boleto.txt")).to include('Pendente')
-    expect(File.read("data/#{Time.now.to_i}_Boleto.txt")).to include('25/04/2020')
-    expect(File.read("data/#{Time.now.to_i}_Boleto.txt")).to include('Boleto')
+    expect(File.read("data/#{Time.now.to_i}_Boleto_emissao.txt")).to include('b7d933ca217964802cce9abc9a75da96372fbfef')
+    expect(File.read("data/#{Time.now.to_i}_Boleto_emissao.txt")).to include('Pendente')
+    expect(File.read("data/#{Time.now.to_i}_Boleto_emissao.txt")).to include('25/04/2020')
+    expect(File.read("data/#{Time.now.to_i}_Boleto_emissao.txt")).to include('Boleto')
   end
 
   it 'Converte 1 json em 1 objeto e salva em txt' do
     invoice = Fatura.to_convert('invoices-iugu/fatura003.json')
     
-    expect(File.read("data/#{Time.now.to_i}_Pix.txt")).to include('a7baa680f588afa01406d236f93a602008baa3f7')
-    expect(File.read("data/#{Time.now.to_i}_Pix.txt")).to include('Pendente')
-    expect(File.read("data/#{Time.now.to_i}_Pix.txt")).to include('25/04/2020')
-    expect(File.read("data/#{Time.now.to_i}_Pix.txt")).to include('Pix')
+    expect(File.read("data/#{Time.now.to_i}_Pix_emissao.txt")).to include('a7baa680f588afa01406d236f93a602008baa3f7')
+    expect(File.read("data/#{Time.now.to_i}_Pix_emissao.txt")).to include('Pendente')
+    expect(File.read("data/#{Time.now.to_i}_Pix_emissao.txt")).to include('25/04/2020')
+    expect(File.read("data/#{Time.now.to_i}_Pix_emissao.txt")).to include('Pix')
   end
 end
